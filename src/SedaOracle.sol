8f81019d6f1140821ef84419ac49994aaed94a27
Jameson Hodge <contact@jamesonhodge.com>
2023-07-10 09:14:50 -0400

    feat: create functions for posting/reading dr results

--- src/SedaOracle.sol
+++ src/SedaOracle.sol
@@ -1,19 +1,70 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-contract SedaOracle {
-    uint256 public data_request_count;
-    mapping(uint256 => DataRequest) public data_requests;
-
+library SedaOracleLib {
     struct DataRequest {
+        uint128 dr_id;
         string value;
     }
 
-    event DataRequestPosted(uint256 id, string value);
+    struct DataResult {
+        uint128 dr_id;
+        string value;
+        string result;
+    }
+}
 
-    function postDataRequest(string memory _value) public {
+contract SedaOracle {
+    uint128 public data_request_count;
+    mapping(uint128 => SedaOracleLib.DataRequest) public data_request_pool;
+    mapping(uint128 => SedaOracleLib.DataResult) public data_results;
+
+    event DataRequestPosted(uint128 id, string value);
+    event DataResultPosted(uint128 id, string value, string result);
+
+    error DataRequestNotFound(uint128 id);
+
+    /// @notice Get a data request by id
+    /// @dev Throws if the data request does not exist
+    function getDataRequest(uint128 id) public view returns (SedaOracleLib.DataRequest memory) {
+        SedaOracleLib.DataRequest memory data_request = data_request_pool[id];
+        if (data_request.dr_id == 0) {
+            revert DataRequestNotFound(id);
+        }
+        return data_request;
+    }
+
+    /// @notice Get an array of data requests starting from a position, up to a limit
+    function getDataRequests(uint128 position, uint128 limit)
+        public
+        view
+        returns (SedaOracleLib.DataRequest[] memory)
+    {
+        // starting from position, iterate forwards until we reach the limit or the end of the data requests
+        SedaOracleLib.DataRequest[] memory data_requests = new SedaOracleLib.DataRequest[](limit);
+        uint128 count = 0;
+        for (uint128 i = position; i <= data_request_count; i++) {
+            if (count == limit) {
+                break;
+            }
+            data_requests[count] = getDataRequest(i);
+            count++;
+        }
+        return data_requests;
+    }
+
+    /// @notice Post a data request
+    function postDataRequest(string calldata _value) public {
         data_request_count++;
-        data_requests[data_request_count] = DataRequest(_value);
+        data_request_pool[data_request_count] = SedaOracleLib.DataRequest(data_request_count, _value);
         emit DataRequestPosted(data_request_count, _value);
+    }
+
+    /// @notice Post a result for a data request
+    function postDataResult(uint128 dr_id, string calldata result) public {
+        SedaOracleLib.DataRequest memory data_request = getDataRequest(dr_id);
+        data_results[dr_id] = SedaOracleLib.DataResult(dr_id, data_request.value, result);
+        delete data_request_pool[dr_id];
+        emit DataResultPosted(dr_id, data_request.value, result);
     }
 }
--- test/SedaOracle.t.sol
+++ test/SedaOracle.t.sol
@@ -15,6 +15,50 @@ contract SedaOracleTest is Test {
         assertEq(oracle.data_request_count(), 0);
         oracle.postDataRequest("test");
         assertEq(oracle.data_request_count(), 1);
-        assertEq(oracle.data_requests(1), "test");
+        (uint128 expected_id, string memory expected_value) = oracle.data_request_pool(1);
+        assertEq(expected_id, 1);
+        assertEq(expected_value, "test");
+    }
+
+    function testPostDataResult() public {
+        oracle.postDataRequest("test");
+        (uint128 dr_id, string memory dr_value) = oracle.data_request_pool(1);
+        (uint128 res_id, string memory res_value, string memory res_result) = oracle.data_results(1);
+        assertEq(dr_id, 1);
+        assertEq(dr_value, "test");
+        assertEq(res_id, 0);
+        assertEq(res_value, "");
+        assertEq(res_result, "");
+
+        oracle.postDataResult(1, "result");
+        (uint128 dr_id_after, string memory dr_value_after) = oracle.data_request_pool(1);
+        (uint128 res_id_after, string memory res_value_after, string memory res_result_after) = oracle.data_results(1);
+        assertEq(dr_id_after, 0);
+        assertEq(dr_value_after, "");
+        assertEq(res_id_after, 1);
+        assertEq(res_value_after, "test");
+        assertEq(res_result_after, "result");
+    }
+
+    function getDataRequests() public {
+        oracle.postDataRequest("1");
+        oracle.postDataRequest("2");
+        oracle.postDataRequest("3");
+
+        // fetch all three data requests
+        SedaOracleLib.DataRequest[] memory data_requests = oracle.getDataRequests(1, 3);
+        assertEq(data_requests.length, 3);
+
+        // fetch data requests with a limit of 2
+        SedaOracleLib.DataRequest[] memory data_requests_2 = oracle.getDataRequests(1, 2);
+        assertEq(data_requests_2.length, 2);
+
+        // fetch a single data request
+        SedaOracleLib.DataRequest[] memory data_requests_3 = oracle.getDataRequests(1, 1);
+        assertEq(data_requests_3.length, 1);
+
+        // fetch two data requests starting from the second index
+        SedaOracleLib.DataRequest[] memory data_requests_4 = oracle.getDataRequests(2, 2);
+        assertEq(data_requests_4.length, 1);
     }
 }
