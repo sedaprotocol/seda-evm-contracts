a59039d20bd41c107234b6a726738dd405099669
Mario Cao <mariocao@gmail.com>
2024-10-03 11:39:41 +0000

    chore: add ci workflow

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,26 +6,26 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: '0.8.25',
-  networks: {
-    baseSepolia: {
-      accounts: [process.env.EVM_PRIVATE_KEY || ''],
-      url: 'https://sepolia.base.org',
-      chainId: 84532,
-    },
-  },
-  etherscan: {
-    apiKey: process.env.BASE_SEPOLIA_ETHERSCAN_API_KEY || '',
-    customChains: [
-      {
-        chainId: 84532,
-        network: 'baseSepolia',
-        urls: {
-          apiURL: 'https://api-sepolia.basescan.org/api',
-          browserURL: 'https://sepolia.basescan.org',
-        },
-      },
-    ],
-  },
+  // networks: {
+  //   baseSepolia: {
+  //     accounts: [process.env.EVM_PRIVATE_KEY || ''],
+  //     url: 'https://sepolia.base.org',
+  //     chainId: 84532,
+  //   },
+  // },
+  // etherscan: {
+  //   apiKey: process.env.BASE_SEPOLIA_ETHERSCAN_API_KEY || '',
+  //   customChains: [
+  //     {
+  //       chainId: 84532,
+  //       network: 'baseSepolia',
+  //       urls: {
+  //         apiURL: 'https://api-sepolia.basescan.org/api',
+  //         browserURL: 'https://sepolia.basescan.org',
+  //       },
+  //     },
+  //   ],
+  // },
 };
 
 export default config;
