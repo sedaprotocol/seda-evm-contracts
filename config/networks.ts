4529b7dd50e8ef3fbf8c4e5878b78ada428762ea
Mario Cao <mariocao@gmail.com>
2024-10-07 09:18:36 +0000

    chore: comment baseSepolia network

--- config/networks.ts
+++ config/networks.ts
@@ -1,14 +1,14 @@
 import type { Networks } from './types';
 
 export const networks: Networks = {
-  baseSepolia: {
-    accounts: 'EVM_PRIVATE_KEY',
-    chainId: 84532,
-    url: 'https://sepolia.base.org',
-    etherscan: {
-      apiKey: 'BASE_SEPOLIA_ETHERSCAN_API_KEY',
-      apiUrl: 'https://api-sepolia.basescan.org/api',
-      explorerUrl: 'https://sepolia.basescan.org',
-    },
-  },
+  // baseSepolia: {
+  //   accounts: 'EVM_PRIVATE_KEY',
+  //   chainId: 84532,
+  //   url: 'https://sepolia.base.org',
+  //   etherscan: {
+  //     apiKey: 'BASE_SEPOLIA_ETHERSCAN_API_KEY',
+  //     apiUrl: 'https://api-sepolia.basescan.org/api',
+  //     explorerUrl: 'https://sepolia.basescan.org',
+  //   },
+  // },
 };
