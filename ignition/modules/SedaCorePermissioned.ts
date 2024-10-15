891cb5eddebeba36a930cc4f313a4f8f5fdfdbaf
Mario Cao <mariocao@gmail.com>
2024-10-15 13:28:08 +0000

    chore: add ignition script for permissioned

---
+++ ignition/modules/SedaCorePermissioned.ts
@@ -0,0 +1,23 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+const SedaProverModule = buildModule('SedaCorePermissioned', (m) => {
+  // Constructor arguments
+  const relayers = [m.getAccount(0)];
+  const maxReplicationFactor = m.getParameter('maxReplicationFactor', 1);
+
+  // Deploy SedaDataTypes library
+  const dataTypesLib = m.library('SedaDataTypes');
+
+  // Deploy SedaCorePermissioned contract with the library
+  const coreContract = m.contract(
+    'SedaCorePermissioned',
+    [relayers, maxReplicationFactor],
+    {
+      libraries: { SedaDataTypes: dataTypesLib },
+    }
+  );
+
+  return { coreContract };
+});
+
+export default SedaProverModule;
