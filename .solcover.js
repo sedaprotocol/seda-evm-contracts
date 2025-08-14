6a384753b7af102d4a57781cf1f1a48db9df600b
Mario Cao <mariocao@gmail.com>
2025-08-14 13:13:27 +0200

    test: check non-owner upgrading proxy

--- .solcover.js
+++ .solcover.js
@@ -3,6 +3,6 @@ module.exports = {
     grep: '[Gg]as.*[Aa]nalysis',
     invert: true,
   },
-  skipFiles: ['mocks/', 'interfaces/', 'libraries/SedaDataTypes.sol', 'test/'],
+  skipFiles: ['mocks/', 'interfaces/', 'libraries/', 'test/'],
   modifierWhitelist: ['initializer', 'onlyInitializing'],
 };
