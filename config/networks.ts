5bc181b9f1371b9679487227a53a9ffee6c0824e
jasperdg <jasperdegooijer@gmail.com>
2025-12-16 15:42:24 +0100

    feat: added mantra deployment (#122)
    * feat: added mantra deployment
    
    * feat: added mantra with verification

--- config/networks.ts
+++ config/networks.ts
@@ -57,6 +57,16 @@ export const networks: Networks = {
       browserUrl: 'https://hyperevmscan.io/',
     },
   },
+  mantra: {
+    accounts: 'EVM_PRIVATE_KEY',
+    chainId: 5888,
+    url: 'https://evm.archive.mantrachain.io/',
+    etherscan: {
+      apiKey: 'NO_API_KEY',
+      apiUrl: 'https://blockscout.mantrascan.io/api',
+      browserUrl: 'https://blockscout.mantrascan.io/',
+    },
+  },
   plume: {
     accounts: 'EVM_PRIVATE_KEY',
     chainId: 98866,
@@ -132,7 +142,12 @@ export const networks: Networks = {
   mantraDukong: {
     accounts: 'EVM_PRIVATE_KEY',
     chainId: 5887,
-    url: 'https://evm.dukong.mantrachain.io',
+    url: 'https://evm.archive.dukong.mantrachain.io',
+    etherscan: {
+      apiKey: 'NO_API_KEY',
+      apiUrl: 'https://explorer.dukong.io/api',
+      browserUrl: 'https://explorer.dukong.io/',
+    },
   },
   neonDevnet: {
     accounts: 'EVM_PRIVATE_KEY',
